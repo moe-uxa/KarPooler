2b1c0436ec84fe9aaed33f2668e3136ba05279d3
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-10-20 22:21:59 +0200

    create splash screen activity

--- karpooler-android-app/app/src/main/java/com/eramo/karpooler/activities/SplashScreenActivity.java
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/activities/SplashScreenActivity.java
@@ -1,14 +1,32 @@
 package com.eramo.karpooler.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.eramo.karpooler.R;
 
 public class SplashScreenActivity extends BaseActivity {
 
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                //Intent mainIntent = new Intent(SplashScreenActivity.this, Menu.class);
+                //SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
