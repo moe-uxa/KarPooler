5b8973535dc5fe38e0b41eb9e97e45cc49bae7d2
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-10-23 18:14:22 +0200

    complete facebook settings

--- karpooler-android-app/app/src/main/java/com/eramo/karpooler/activities/LoginActivity.java
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.eramo.karpooler.activities;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,10 +11,13 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +34,16 @@ public class LoginActivity extends AppCompatActivity {
         // If using in a fragment
         //loginButton.setFragment(this);
 
+
         // create call back manager
-        CallbackManager callbackManager = CallbackManager.Factory.create();
+        callbackManager = CallbackManager.Factory.create();
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                Log.i("xxxx", "Access Token: "+ loginResult.getAccessToken());
+
             }
 
             @Override
@@ -52,5 +57,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
