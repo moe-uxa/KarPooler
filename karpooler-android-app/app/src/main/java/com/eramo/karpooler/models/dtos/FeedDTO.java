bfcc47d1fb3221cf53e69529963ab1158f5b6820
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-10-31 21:18:13 +0200

    Feed Tab: create feeds models

--- karpooler-android-app/app/src/main/java/com/eramo/karpooler/models/dtos/FeedDTO.java
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/models/dtos/FeedDTO.java
@@ -1,7 +1,84 @@
 package com.eramo.karpooler.models.dtos;
 
+import android.graphics.Bitmap;
+
+import java.util.List;
+
 /**
  * Created by Mohamed.Gaber on 10/30/2015.
  */
 public class FeedDTO {
+
+    private int feedId;
+    private Bitmap ownerImage;
+    private String ownerName;
+    private String feedTitle;
+    private String feedTime;
+    private int numberOfFriendsInCommon;
+    private Bitmap [] socialMediasList;
+    private String feedDescription;
+
+    public int getFeedId() {
+        return feedId;
+    }
+
+    public void setFeedId(int feedId) {
+        this.feedId = feedId;
+    }
+
+    public Bitmap getOwnerImage() {
+        return ownerImage;
+    }
+
+    public void setOwnerImage(Bitmap ownerImage) {
+        this.ownerImage = ownerImage;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getFeedTitle() {
+        return feedTitle;
+    }
+
+    public void setFeedTitle(String feedTitle) {
+        this.feedTitle = feedTitle;
+    }
+
+    public String getFeedTime() {
+        return feedTime;
+    }
+
+    public void setFeedTime(String feedTime) {
+        this.feedTime = feedTime;
+    }
+
+    public int getNumberOfFriendsInCommon() {
+        return numberOfFriendsInCommon;
+    }
+
+    public void setNumberOfFriendsInCommon(int numberOfFriendsInCommon) {
+        this.numberOfFriendsInCommon = numberOfFriendsInCommon;
+    }
+
+    public Bitmap[] getSocialMediasList() {
+        return socialMediasList;
+    }
+
+    public void setSocialMediasList(Bitmap[] socialMediasList) {
+        this.socialMediasList = socialMediasList;
+    }
+
+    public String getFeedDescription() {
+        return feedDescription;
+    }
+
+    public void setFeedDescription(String feedDescription) {
+        this.feedDescription = feedDescription;
+    }
 }
--- karpooler-android-app/app/src/main/java/com/eramo/karpooler/models/dtos/TripCreationFeedDTO.java
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/models/dtos/TripCreationFeedDTO.java
@@ -1,7 +1,31 @@
 package com.eramo.karpooler.models.dtos;
 
+import android.graphics.Bitmap;
+
+import java.util.List;
+
 /**
  * Created by Mohamed.Gaber on 10/30/2015.
  */
+
 public class TripCreationFeedDTO extends FeedDTO {
+
+    private int tripId;
+    private List<Bitmap> tripPassengers;
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(int tripId) {
+        this.tripId = tripId;
+    }
+
+    public List<Bitmap> getTripPassengers() {
+        return tripPassengers;
+    }
+
+    public void setTripPassengers(List<Bitmap> tripPassengers) {
+        this.tripPassengers = tripPassengers;
+    }
 }
