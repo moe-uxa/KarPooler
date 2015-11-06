5c554576d5b203b9761bea0c563b097f877e0b19
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-11-06 18:44:20 +0200

    Trip Details Tab: add map directions successfully

--- karpooler-android-app/app/src/main/java/com/eramo/karpooler/fragments/TripDetailsFragment.java
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/fragments/TripDetailsFragment.java
@@ -1,18 +1,40 @@
 package com.eramo.karpooler.fragments;
 
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.directions.route.Route;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
 import com.eramo.karpooler.R;
+import com.eramo.karpooler.activities.ViewTripActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TripDetailsFragment extends Fragment {
+public class TripDetailsFragment extends Fragment implements OnMapReadyCallback, RoutingListener {
+
+    private GoogleMap mMap;
+    private ViewTripActivity activity;
+
+    private LatLng start = new LatLng(30.067918, 31.228790);
+    private LatLng end = new LatLng(30.122430, 31.399765);
 
 
     public TripDetailsFragment() {
@@ -27,5 +49,76 @@ public class TripDetailsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_trip_details, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // get Activity instance
+        activity = (ViewTripActivity) getActivity();
+
+        // add map fragment to map container
+        SupportMapFragment mMapFragment = SupportMapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map_container, mMapFragment);
+        fragmentTransaction.commit();
+
+        // get notified when the map is ready to be used.
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLngBounds area = new LatLngBounds(start, end);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(area, 400, 300, 0));
+
+        Routing routing = new Routing.Builder()
+                .travelMode(Routing.TravelMode.DRIVING)
+                .withListener(this)
+                .alternativeRoutes(true)
+                .waypoints(start, end)
+                .build();
+        routing.execute();
+
+    }
+
+    @Override
+    public void onRoutingFailure() {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> arrayList, int i) {
+
+        PolylineOptions polyOptions = new PolylineOptions();
+        polyOptions.color(getResources().getColor(R.color.colorMapRoute));
+        polyOptions.width(10);
+        polyOptions.addAll(arrayList.get(i).getPoints());
+        mMap.addPolyline(polyOptions);
+
+        // marker icon from resource
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.ic_map_marker);
+
+        // add marker at start
+        mMap.addMarker(new MarkerOptions().position(start).icon(icon));
+
+        // add marker at end
+        mMap.addMarker(new MarkerOptions().position(end). icon(icon));
+
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
 
 }
