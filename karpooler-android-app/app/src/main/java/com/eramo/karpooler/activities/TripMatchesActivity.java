1896e65493905f62df2c13730ff6f6079b0acc0e
Mohamed Gaber <m.gaber.ali92@gmail.com>
2015-11-08 18:15:11 +0200

    create trip matches activity

---
+++ karpooler-android-app/app/src/main/java/com/eramo/karpooler/activities/TripMatchesActivity.java
@@ -0,0 +1,92 @@
+package com.eramo.karpooler.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eramo.karpooler.R;
+import com.eramo.karpooler.fragments.TripMatchesFragment;
+
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+public class TripMatchesActivity extends AppCompatActivity implements MaterialTabListener{
+
+    private MaterialTabHost tabHost;
+    private ViewPager pager;
+    private ViewPagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_matches);
+
+        tabHost = (MaterialTabHost) this.findViewById(R.id.materialTabHost);
+        pager = (ViewPager) this.findViewById(R.id.pager);
+
+        // init view pager
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // when user do a swipe the selected tab change
+                tabHost.setSelectedNavigationItem(position);
+            }
+        });
+
+        // insert all tabs from pagerAdapter data
+        // insert all tabs from pagerAdapter data
+        for (int i = 0; i < pagerAdapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(pagerAdapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
+
+        }
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+
+        public Fragment getItem(int num) {
+            return new TripMatchesFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 7;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Section " + position;
+        }
+
+    }
+
+}
